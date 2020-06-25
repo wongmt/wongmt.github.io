31dea07c49d56682323117fe966398329edcbe77
M Wong <eemtwong@gmail.com>
2020-06-25 11:51:01 +1000

    updated on 25-6-20

--- mybutton.js
+++ mybutton.js
@@ -2,6 +2,7 @@
 
 const e = React.createElement;
 
+
 class MyButton extends React.Component {
 
   constructor(props) {
@@ -14,12 +15,14 @@ class MyButton extends React.Component {
     if (this.state.accepted) {
 		document.getElementById("notice").style.display="none";
 		document.cookie="myacceptbutton1=yes";
+		return("");
     }
 
     return e(
+	    
       'button',
       { onClick: () => this.setState({ accepted: true }) },
-      'Agree'
+      'Similar to many other websites, this site uses cookies in order to provide better web surfing experience for visitors. If you continue to use this site, it will be assumed that you agree to the use of cookies. Please click HERE if you accept the use of cookies and continue browsing in this site. You can read more details about the privacy information in the navigation menu at the top if interested.'
     );
   }
 }
