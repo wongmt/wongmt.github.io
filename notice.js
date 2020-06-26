61fe1f7d13013b9292803c33ec1c4fc274fbed71
wongmt <66844116+wongmt@users.noreply.github.com>
2020-06-27 01:25:31 +1000

    Update notice.js

--- notice.js
+++ notice.js
@@ -5,8 +5,8 @@ class Notice extends React.Component {
 
     render() {
 		return (	        
-		'Similar to many other websites, this site uses cookies in order to the performance of the website and provide better '+
-		'web surfing experience for site visitors. If you continue to use this site, it will be assumed that you agree to the use of '+
+		'Similar to many other websites, this site uses cookies in order to improve the performance of the website and provide better '+
+		'web surfing experience for the site visitors. If you continue to use this site, it will be assumed that you agree to the use of '+
 		'cookies. You can read more details about the privacy policy in the navigation menu at the top if you are interested.');
 	}
 }
