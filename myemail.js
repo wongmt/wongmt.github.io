e5b19018cfa7dd38efea3c0cb17fa18cf501125b
wongmt <66844116+wongmt@users.noreply.github.com>
2020-06-28 08:53:20 +1000

    Update myemail.js

--- myemail.js
+++ myemail.js
@@ -3,7 +3,10 @@
 class MyEmail extends React.Component {
 
     render() {
-		return 'My contact email: mwong(at)mwongsite.com, replace (at) by @';
+		return('My contact email is mwong(at)mwongsite.com, replace (at) by @ ' + 
+		       'If you contact the site author by email, your name, email address '+
+		       'and other information you provide to the site author will be stored '+
+		       'in email account of the site author.';
 	}
 } 
 
