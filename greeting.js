dd8cffb247493f2533c4f84cfe18a4434030ba3e
wongmt <66844116+wongmt@users.noreply.github.com>
2020-06-28 05:24:32 +1000

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -4,7 +4,9 @@ class Greeting extends React.Component {
 
     render() {
 		return ('Welcome to my personal website. I am a researcher in computer vision & '+
-			'machine learning with Ph.D in Computing, M.Sc & B.Sc(Hons.) in Electronic Engineering.');
+			'machine learning with Ph.D in Computing, M.Sc & B.Sc(Hons.) in Electronic Engineering. '+
+		        'I have experience in Python, Java, C++, PHP and Javascript. I also have stong interest ' +
+		        'in web app development and I have experience in Django, Flask, React JS and CodeIgniter.');
 	}
 } 
 
