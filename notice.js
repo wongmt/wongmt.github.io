b264458b3751ac4403ce0660d1d024edfcf06c8d
wongmt <66844116+wongmt@users.noreply.github.com>
2020-06-29 08:55:31 +1000

    Update notice.js

--- notice.js
+++ notice.js
@@ -5,7 +5,7 @@ var app = new Vue({
 			'order to improve the performance of the website and provide '+
 			'better web surfing experience for the site visitors. If you '+
 			'continue to use this site, it will be assumed that you agree '+
-			'to the use of cookies. You can read more details about the '+
+			'to the use of cookies. Please click the Agree button below to continue browsing. You can read more details about the '+
 			'privacy policy in the navigation menu at the top if you are interested.'	
   }
 })
