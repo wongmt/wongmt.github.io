2da442ea9748aa1fe6da7589a9c92fd461c72a2d
wongmt <66844116+wongmt@users.noreply.github.com>
2020-06-29 04:47:11 +1000

    Update myemail.js

--- myemail.js
+++ myemail.js
@@ -1,14 +1,11 @@
-'use strict';
-
-class MyEmail extends React.Component {
-
-    render() {
-		return('My contact email is mwong(at)mwongsite.com, replace (at) by @ ' + 
-		       'If you contact the site author by email, your name, email address '+
-		       'and other information you provide to the site author will be stored '+
-		       'in email account of the site author.');
-	}
-} 
-
-const emailContainer = document.querySelector('#myemail_container');
-ReactDOM.render(React.createElement(MyEmail), emailContainer);
+var myemail_app = new Vue({
+  el: '#myemail_container',
+  data: {
+    	email_msg1: 	'Email: mwong(at)mwongsite.com, replace (at) by @',
+	email_msg2: 	'If you contact the site author by email, your name, email address and other information '+
+			'you provide to the site author will be stored in the email account of the site author. ' +
+	  		'This is necessary because the site author can refer to these information to answer your '+
+	  		'queries if you have any more questions to the site author in the future.'
+			
+  }
+})
