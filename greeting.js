cca708cd23f493cfbfff6d978479a77c6c3b7d02
wongmt <66844116+wongmt@users.noreply.github.com>
2020-06-29 07:35:06 +1000

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -3,10 +3,10 @@ var app = new Vue({
   data: {
     msg1: 'Home',
 	msg2: 'Welcome to my personal website.',
-	msg3: 	'I am a researcher in computer vision & '+
+	msg3: 	'I am a researcher in computer vision and '+
 		'machine learning with Ph.D in Computing, M.Sc & B.Sc(Hons.) '+
 		'in Electronic Engineering. I have experience in Python, Java, '+
-		'C++, PHP and Javascript. I also have stong interest in web app '+
+		'C++, PHP and Javascript. I also have strong interest in web app '+
 		'development and I have experience in Django, Flask, React JS, '+
 		'Vue JS and CodeIgniter. There are two purposes of this site. '+
 	  	'One purpose is to show breifly my personal information in this site and the '+
