bc23907624c7c1484d3fb0062998d89045658e82
M Wong <eemtwong@gmail.com>
2020-08-17 04:57:33 +1000

    updated in 17-8

--- content.js
+++ content.js
@@ -4,19 +4,21 @@ var app = new Vue({
 	content: 	'Welcome to my personal website. I am a researcher in ' +
 				'computer vision and machine learning with Ph.D in ' +
 				'Computing, M.Sc & B.Sc(Hons.) in Electronic Engineering. ' +
-				'I have experience in Python, Java, C++, PHP and Javascript. ' +
+				'I have experience in Python, Java, C++, PHP, Javascript and Kotlin. ' +
 				'I also have strong interest in web app development using Python ' +
-				'and Javascript. There are two purposes of this site. ' +
+				'and Javascript, and mobile app development using Java and Kotlin. ' +
+				'There are two purposes of this site. ' +
 				'One purpose is to show breifly my personal information in '+
 				'this site and the other purpose is to use this site to learn '+
 				'Javascript framework. This site is built by using the Vue '+
 				'Javascript framework, Javascript, HTML and CSS.',
 	skills_title: 'Experience and skills',
     skills:[		
-		{text: 'Programming languages: Python, Javascript, C++, Java, PHP'},
+		{text: 'Programming languages: Python, Javascript, C++, Java, Kotlin, PHP'},
 		{text: 'Web frameworks: Django, Flask, React JS, Vue JS and CodeIgniter'},
 		{text: 'Computer vision software libraries: OpenCV, scikit-image'},
-		{text: 'Machine learning software libraries: Tensorflow, Weka'}]
+		{text: 'Machine learning software libraries: Tensorflow, Weka'},
+		{text: 'Mobile app development: Android Studio'}]
 		}
 })
 		
