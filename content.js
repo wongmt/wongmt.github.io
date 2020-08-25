d41dd7d062d679c1441e12e1d43ecd3e4aaa9d40
M Wong <eemtwong@gmail.com>
2020-08-25 11:12:19 +1000

    updated in 25-8

--- content.js
+++ content.js
@@ -1,7 +1,8 @@
 var app = new Vue({
   el: '#content_container',
   data: {
-	content: 	'Welcome to my personal website. I am a researcher in ' +
+	content: 	'<div><h1>Home</h1>'+
+				'<p>Welcome to my personal website. I am a researcher in ' +
 				'computer vision and machine learning with Ph.D in ' +
 				'Computing, M.Sc & B.Sc(Hons.) in Electronic Engineering. ' +
 				'I have experience in Python, Java, C++, PHP, Javascript and Kotlin. ' +
@@ -11,14 +12,12 @@ var app = new Vue({
 				'One purpose is to show breifly my personal information in '+
 				'this site and the other purpose is to use this site to learn '+
 				'Javascript framework. This site is built by using the Vue '+
-				'Javascript framework, Javascript, HTML and CSS.',
-	skills_title: 'Experience and skills',
-    skills:[		
-		{text: 'Programming languages: Python, Javascript, C++, Java, Kotlin, PHP'},
-		{text: 'Web frameworks: Django, Flask, React JS, Vue JS and CodeIgniter'},
-		{text: 'Computer vision software libraries: OpenCV, scikit-image'},
-		{text: 'Machine learning software libraries: Tensorflow, Weka'},
-		{text: 'Mobile app development: Android Studio'}]
+				'Javascript framework, Javascript, HTML and CSS.</p>'+
+				'<h3>Experience and skills</h3>' +
+				'<ul><li>Programming languages: Python, Javascript, C++, Java, Kotlin, PHP</li>'+
+				'<li>Web frameworks: Django, Flask, ReactJS, VueJS and CodeIgniter</li>'+
+				'<li>Computer vision software libraries: OpenCV, scikit-image</li>'+
+				'<li>Machine learning software libraries: Tensorflow, Weka</li>'+
+				'<li>Mobile app development: Android Studio</li></ul></div>'
 		}
 })
-		
