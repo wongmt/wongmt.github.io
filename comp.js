cf6b8b26b555f932aa25d73c64b04be46def4879
M Wong <eemtwong@gmail.com>
2020-09-17 19:20:00 +1000

    updated in 17-9-2020

--- comp.js
+++ comp.js
@@ -1,13 +1,7 @@
-Vue.component('headercomponent',{
-   template : '<div><div id="header"><h1>M Wong</h1></div>	
-				<div id ="nav">
-				<a href="index.html">Home</a> |
-				<a href="privacy.html">Privacy</a>
-				</div></div>'
-});
+Vue.component('navcomponent', {
+		template: '<div id="nav"><a href="index.html">Home</a> | <a href="privacy.html">Privacy</a></div>'
+})
+
 var vm = new Vue({
-   el: '#head'
-});
-var vm1 = new Vue({
-   el: '#head1'
-});
+   el: '#mynav'
+})
