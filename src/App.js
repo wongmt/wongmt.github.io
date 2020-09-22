ca31b414f1048113b3b2e22fbb80eafb624c40a6
M Wong <eemtwong@gmail.com>
2020-09-23 04:52:09 +1000

    updated 23-9

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ function App() {
         <h1>M. Wong</h1>
         <h3>My Personal Site</h3>
 		</div>
+		<p>	Welcome to my personal website. I am a researcher in 
+			computer vision and machine learning with Ph.D in 
+			Computing, M.Sc & B.Sc(Hons.) in Electronic Engineering.
+			I have experience in Python, Java, C++, PHP, Javascript and Kotlin.
+			I also have strong interest in web app development using Python 
+			and Javascript, and mobile app development using Java and Kotlin. 
+			This site is built by using the Vue Javascript framework, Javascript,
+			HTML and CSS. This site is hosted by GitHub Pages.</p>
+			
+			<h2>Experience and skills</h2>
+			<ul><li>Programming languages: Python, Javascript, C++, Java, Kotlin, PHP</li>
+			<li>Web frameworks: Django, Flask, ReactJS, VueJS and CodeIgniter</li>
+			<li>Computer vision software libraries: OpenCV, scikit-image</li>
+			<li>Machine learning software libraries: Tensorflow, Weka</li>
+			<li>Mobile app development: Kotlin, Java, Android Studio</li></ul>
 		<br />
         <p><a className="App-link"
           href="https://wongmt.github.io/mysite/cv.html" >My CV</a></p>
