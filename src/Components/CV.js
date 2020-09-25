ab575bca3bc0f33bda262ea5dc30453b0cfffc2b
M Wong <eemtwong@gmail.com>
2020-09-26 02:23:13 +1000

    26-9-20

---
+++ src/Components/CV.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function CV() {
+  return (
+    <div className="CV">	
+	<h2>Experience & skills</h2>
+			<ul><li>Programming languages: Python, Javascript, C++, Java, Kotlin, PHP</li>
+			<li>Web frameworks: Django, Flask, ReactJS, VueJS and CodeIgniter</li>
+			<li>Computer vision software libraries: OpenCV, scikit-image</li>
+			<li>Machine learning software libraries: Tensorflow, Weka</li>
+			<li>Mobile app development: Kotlin, Java, Android Studio</li></ul>
+	</div>
+);
+}
+export default CV;	
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+	  
+        <div className="MyHeader">
+        <h1>M. Wong</h1>
+        <h3>My Personal Site</h3>
+		</div>
+        
+	</div>
+		
+  );
+}
+export default Header;
