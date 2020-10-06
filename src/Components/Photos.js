d798bb98ae462607f53037e85d3a0b69034d0c7f
M Wong <eemtwong@gmail.com>
2020-10-06 22:31:01 +1100

    Updated in 6-10-20

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -6,7 +6,9 @@ function Photos() {
     <div className="Photo">
 	<h1>Photos</h1>
 	<h3>Australian Catholic University, Strathfield campus, NSW, Australia</h3>
-	<img src={require('../acu5.jpg')} alt="Photo image" />
+	<p>Date: 6-10-2020</p>
+	<p>The photo was taken in 2018 and it was uploaded to this site in 6-10-2020.</p>
+	<img src={require('./images/acujs.jpg')} alt="Photo image" />
 	</div>
   );
 }
