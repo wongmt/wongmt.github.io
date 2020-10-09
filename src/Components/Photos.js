52be8f876147293c7f26b7f69072ef237228b664
M Wong <eemtwong@gmail.com>
2020-10-09 21:00:01 +1100

    updated in 9-10-20

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -1,10 +1,16 @@
 import React from 'react';
-//import '../App.css';
 
 function Photos() {
   return (
     <div className="Photo">
 	<h1>Photos</h1>
+	<h3>UTS Haymarket Building 5, University of Technology Sydney</h3>
+	<p>Date: 9-10-2020</p>
+	<p>UTS Haymarket Building 5 of University of Technology Sydney, in city of 
+	   Sydney, NSW, Australia.</p>
+	<img src={require('./images/uts-bldg5.jpg')} alt="Photo image" />
+	<br /><br />
+	
 	<h3>Homebush Public School, Homebush, NSW, Australia</h3>
 	<p>Date: 7-10-2020</p>
 	<p>The photo was taken in 2018 and it was uploaded to this site in 7-10-2020.</p>
