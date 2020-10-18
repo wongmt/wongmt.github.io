da71b59139e4d6d8119de02849e3829d0df1a694
M Wong <eemtwong@gmail.com>
2020-10-18 12:56:53 +1100

    18-10

--- myheadercom.js
+++ myheadercom.js
@@ -2,6 +2,7 @@ Vue.component('myheadercom', {
 		template: '<div><div id="header"><h1>M Wong</h1></div>'+
 					'<div id="nav"><a href="index.html">Home</a> | '+
 					'<a href="cv.html">Resume</a> | ' +
+					'<a href="photos.html">Photos</a> | ' +
 					'<a href="privacy.html">Privacy</a></div></div>'
 })
 
