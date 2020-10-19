e1c20a566a97cfccfe9b99cda44e81dae061837c
M Wong <eemtwong@gmail.com>
2020-10-20 05:40:20 +1100

    20-10

---
+++ cv.js
@@ -0,0 +1,23 @@
+var cv_app = new Vue({
+  el: '#cv_container',
+  data: {	cv:'<div><h1>Brief CV</h1>'+
+				'<h3>Education</h3>'+
+				'<ul><li>PhD in Computer Science</li>'+
+				'<li>M.Sc in Electronic Engineering</li>'+
+				'<li>B.Sc (Hons.) in Electrical & Electronic Engineering</li>'+
+				'</ul>'+
+				'<h3>Research specialism</h3>'+
+				'<ul>'+
+				'<li>Computer vision</li>'+
+				'<li>Machine learning</li>'+
+				'</ul>'+
+				'<h3>Skills & experience</h3>'+
+				'<ul>'+
+	'<li>Programming languages: Python, Java, JavaScript, C++, Python, Kotlin, PHP</li>'+
+	'<li>Web frameworks: Flask, Django, ReactJS, VueJS, CodeIgniter</li>'+
+	'<li>Machine learning library: Tensorflow, scikit-learn, Weka</li>'+
+	'<li>Computer vision library: OpenCV, scikit-image</li>'+
+	'</ul>'+
+	'</div>'
+		}
+})
