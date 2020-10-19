e0308273ee7c61278fa34da7316a61927263081f
M Wong <eemtwong@gmail.com>
2020-10-20 04:00:29 +1100

    20-10

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -5,7 +5,7 @@ function Photos() {
     <div className="Photo">
 	<h1>Photos</h1>
 	<h3>UTS Haymarket Building 5, University of Technology Sydney</h3>
-	<p>Date: 9-10-2020</p>
+	<p>Date: 19-10-2020</p>
 	<p>UTS Haymarket Building 5 of University of Technology Sydney, in city of 
 	   Sydney, NSW, Australia.</p>
 	<img src={require('./images/uts-bldg5r.jpg')} alt="Photo image" />
