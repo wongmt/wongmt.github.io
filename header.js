d482e0a479cd0e648f525b58808a8b165b0b32eb
M Wong <eemtwong@gmail.com>
2020-10-21 05:48:11 +1100

    21-10

---
+++ header.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#header_container',
+  data: {
+		header:	'<div><div id="header"><h1>M Wong</h1><h3>My personal site</h3></div>'+
+					'<div id="nav"><a href="index.html">Home</a> | '+
+					'<a href="cv.html">Resume</a> | ' +
+					'<a href="photos.html">Photos</a> | ' +
+					'<a href="privacy.html">Privacy</a></div></div>'	
+		}
+})
