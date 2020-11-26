3fcdb5d251c9c68e785f572dd300dc86a2b9b145
M Wong <eemtwong@gmail.com>
2020-11-27 02:02:55 +1100

    27-11

--- src/App.js
+++ src/App.js
@@ -7,27 +7,6 @@ import Home from './Components/Home';
 import Photos from './Components/Photos';
 import { Link, Route, Switch } from "react-router-dom";
 
-
-
-/*
-function App() {
-  return (
-	<div className="App">	
-	<div>
-	<Header name="M Wong" sub_title="My personal site" />
-	<nav className="Links">
-	<Link to="/">HOME</Link> | <Link to="/cv">CV</Link> | <Link to="/photos">Photos</Link> | <Link to="/privacy">Privacy</Link>
-	</nav>
-	<Route exact={true} path="/" component={Home} />
-	<Route exact={true} path="/cv" component={CV} />
-	<Route exact={true} path="/photos" component={Photos} />
-	<Route exact={true} path="/privacy" component={Privacy} />
-	</div>
-
-    </div>	
-  );
-}*/
-
 function App() {
     return (
 	
@@ -40,7 +19,7 @@ function App() {
     <Route path="/" component={Home} exact />
     <Route path="/cv" component={CV} />
     <Route path="/photos" component={Photos} />
-	<Route path="/privacy" component={Privacy} />		
+    <Route path="/privacy" component={Privacy} />		
     </div>
 	</div>
     );
