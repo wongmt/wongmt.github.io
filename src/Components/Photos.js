cffa142df93005a90f7cf2c34f7d8e87e6096820
M Wong <eemtwong@gmail.com>
2020-11-26 22:35:54 +1100

    26-11

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import utsbldg5 from './images/uts-bldg5r.jpg';
+import hps from './images/hps1.jpg';
+import acupy from './images/acupy.jpg';
 
 function Photos() {
   return (
@@ -8,16 +11,16 @@ function Photos() {
 	
 	<p>UTS Haymarket Building 5 of University of Technology Sydney, in city of 
 	   Sydney, NSW, Australia.</p>
-	<img src={require('./images/uts-bldg5r.jpg')} alt="Photo image" />
+	<img src={utsbldg5} alt="UTS" />
 	<br /><br />
 	
 	<h3>Homebush Public School, Homebush, NSW, Australia</h3>
 	<p>It is a primary school in Homebush, NSW, Australia.</p>
-	<img src={require('./images/hps1.jpg')} alt="Photo image" />
+	<img src={hps} alt="Homebush Public School" />
 	<br /><br />
 	
 	<h3>Australian Catholic University, Strathfield campus</h3>	
-	<img src={require('./images/acupy.jpg')} alt="Photo image" />
+	<img src={acupy} alt="ACU" />
 	<br /><br />
 	
 	</div>
