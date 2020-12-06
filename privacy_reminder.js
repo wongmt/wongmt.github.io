9940ccb0ee2ef78db8a46345dd8efaf5c4500ff1
wongmt <66844116+wongmt@users.noreply.github.com>
2020-12-06 20:20:58 +1100

    Update privacy_reminder.js

--- privacy_reminder.js
+++ privacy_reminder.js
@@ -1,5 +1,5 @@
 document.getElementById('notice_container').innerHTML= '<div id="notice">'+
-	'<p>Similar to many other websites, this site uses '+ 
+	'<p>This site uses '+ 
 	'cookies to improve the browsing experience of site visitors. '+ 
 	'If you continue to use this site, it will be assumed you agree '+ 
 	'to the use of cookies. Please click the "Agree" button to '+
