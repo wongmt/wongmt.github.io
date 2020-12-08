19fb1670d79a2d555e20a43a93f19603af5528da
M Wong <eemtwong@gmail.com>
2020-12-09 03:35:59 +1100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# wongmt.github.io
-This is the personal site of M. Wong.
-The site is built by using JavaScript, HTML and CSS.
--- myheader.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('header_container').innerHTML= '<div><div id="header">'+
-	'<h1>M Wong</h1><h3>My personal site</h3></div>'+
-	'<div id="nav"><a href="index.html">Home</a> | '+
-	'<a href="cv.html">Resume</a> | ' +
-	'<a href="photos.html">Photos</a> | ' +
-	'<a href="privacy.html">Privacy</a></div></div>'	
