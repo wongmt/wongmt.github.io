8530d91216154f2039f288d98c1dc904a070c388
M Wong <eemtwong@gmail.com>
2020-12-19 05:29:40 +1100

    19-12

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
 	<div className="App">
 	<Header name="M Wong" sub_title="My personal site" />
 	<nav className="Links">
-	<Link to="/">HOME</Link> | <Link to="/cv">CV</Link> | 
-	<Link to="/photos">Photos</Link> | <Link to="/privacy">Privacy</Link>
+	<Link to="/">HOME</Link> | <Link to="/cv">CV</Link> | <Link to="/photos">Photos</Link> | 
+	<Link to="/privacy"> Privacy</Link>
 	</nav>  
 	<Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/cv" component={CV} />
     <Route exact path="/photos" component={Photos} />
-    <Route exact path="/privacy" component={Privacy} />		
+    <Route exact path="/privacy" component={Privacy} />	
 	</Switch>
     </div>
     );
